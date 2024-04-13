c2079753f67fbc485b99c8ab074c2e1517120b97
Tejas Shah <tejasshah2k19@gmail.com>
2024-04-13 13:59:55 +0530

    constructor - arument

--- Employee.java
+++ Employee.java
@@ -22,12 +22,33 @@ public class Employee {
         System.out.println(accountLocked);
     }
 
+    Employee(){
+        name = "jack";
+    }
+
+    
+    Employee(String data){
+        name = data;
+    }
     public static void main(String[] args) {
         
             Employee e = new Employee();
             e.printValues();
             //assig the values - default values 
             //constructor -> 
-            
-    }
+            //is a special method which must have same name as class name and it must not return anything
+            //it is used to initialize instance variable 
+
+            //we dont have to call constructor manually 
+            //constructor will call when we create instance. 
+
+            //implicit constructor -> java 
+                //no argument 
+            //explicit constructor -> user 
+                //no argument
+                //parameterized 
+
+            Employee x1 = new Employee("rock");
+            x1.printValues();
+        }
 }
